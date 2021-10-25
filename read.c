9f66a3ca2251a7b023649d2e8f3c1c480c80cea1
kelvin-maker <dennismugaog97@gmail.com>
2021-10-25 11:01:34 -0700

    Function to read and execute

---
+++ read.c
@@ -0,0 +1,32 @@
+#iclude "shell.h"
+
+void read_command ( char cmd[], char *par[] )
+
+  char line[1024];
+int count = 0, i = 0, j = 0;
+char *array[100], *pch;
+
+//Read one line
+for ( ;; ) {
+  int c = fgetc (stdin);
+  line[count ++] = (char) c;
+  if (c =='\n') break;
+ }
+if (count == 1) return;
+phc = strtok (line, "\n" );
+
+// parse the line into two words
+
+while (pch != NULL) {
+  array [i++] = strdup (phc);
+  pch = strtok (NULL, "\n");
+ }
+
+// first wordd is the command
+strcpy (cmd, array[0] );
+
+//others are parameters
+
+for (int j = 0; j < i; j++)
+  par[j] = array[j];
+par[i] = NULL; // Null terminate the parameter list

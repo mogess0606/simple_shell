2fb38038ccadc3b9bc05d22aebb1812fb9f3335f
Clem-Tee <clementogol@gmail.com>
2021-10-26 21:19:01 +0300

    Updated files

--- main.c
+++ main.c
@@ -2,43 +2,28 @@
 
 /**
  * main - entry point
- * @ac: arg count
- * @av: arg vector
  *
  * Return: 0 on success, 1 on error
  */
-int main(int ac, char **av)
+
+int main()
 {
-	info_t info[] = { INFO_INIT };
-	int fd = 2;
-
-	asm ("mov %1, %0\n\t"
-			"add $3, %0"
-			: "=r" (fd)
-			: "r" (fd));
-
-	if (ac == 2)
-	{
-		fd = open(av[1], O_RDONLY);
-		if (fd == -1)
-		{
-			if (errno == EACCES)
-				exit(126);
-			if (errno == ENOENT)
-			{
-				_eputs(av[0]);
-				_eputs(": 0: Can't open");
-				_eputs(av[1]);
-				_eputchar('\n');
-				_eputchar(BUF_FLUSH);
-				exit(127);
-			}
-			return (EXIT_FAILURE);
-		}
-		info->readfd = fd;
+	char cmd[100], command[100], *parameters[20];
+	// env var
+	char *envp[] = { (char *) "PATH=/bin", 0 };
+	while ( 1 ) {
+	type_prompt();
+	read_command ( command, parameters );
+	if ( fork() != 0 )
+	wait ( NULL );
+	else{
+		strcpy ( cmd, "/bin/" );
+		strcat ( cmd, command);
+		execve ( cmd, parameters, envp );
 	}
-	populate_env_list(info);
-	read_history(info);
-	hsh(info, av);
-	return (EXIT_SUCCESS);
+	if ( strcmp ( command, "exit") == 0)
+		break;
+	}
+
+	return 0;
 }
